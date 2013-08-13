6ca4e8bf12513815a198395a194e39b244c3c9b1
chasm <charles.munat@gmail.com>
2013-08-13 11:13:02 -0700

    Added puts and console.logs to explain the process.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,22 +1,30 @@
 $(function() {
   var showPartial = function(href) {
+    console.log(">> showPartial called with href = " + href);
+    console.log(">> calling AJAX GET to http://localhost:3000" + href);
     $.ajax({
       method: 'GET',
       url: "http://localhost:3000" + href,
       success: function(html) {
+        console.log(">> Success!");
+        console.log(">> Emptying div.description divs.");
+        console.log($('.description'));
         $('.description').empty();
+        console.log(">> Adding the html to #" + href.replace('/',''));
+        console.log(html);
         $('#' + href.replace('/','')).html(html);
       }
     });
   };
   
   $("a").click(function(event) {
+    console.log(">> Link clicked!");
     var href = $(this).attr("href");
-    
+    console.log(">> href of the link is " + href);
     history.pushState({}, '', href);
-    
+    console.log(">> Pushed " + href + " onto the history (see address bar)");
     showPartial(href);
-    
+    console.log(">> Preventing the page reload!");
     event.preventDefault();
   });
   
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,9 +2,14 @@ class SiteController < ApplicationController
   def index
     @partial = params[:code]
     
+    puts ">> Received code: " + (@partial || "nothing")
+    
     if @partial
+      puts ">> Found @partial (" + @partial + ")"
       if request.xhr?
-        render :partial => "partials/#{params[:code]}"
+        puts ">> It's an AJAX request!"
+        puts ">> Rendering partials/#{@partial}"
+        render :partial => "partials/#{@partial}"
       end
     end
   end
